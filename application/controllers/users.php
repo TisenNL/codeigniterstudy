a2d38994894c6d02fc02397b5ddedef2ef73453d
Tiago Lino <tiagobrasil18@gmail.com>
2022-01-28 08:24:21 -0300

    Adding registration functionality

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -41,6 +41,29 @@ class Users extends CI_Controller
 //        $id = 3;
 //        $this->user_model->delete_users($id);
 //    }
+
+    public function register()
+    {
+        $this->form_validation->set_rules('first_name', 'First Name', 'trim|required|min_length[3]');
+        $this->form_validation->set_rules('last_name', 'Last Name', 'trim|required|min_length[3]');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|min_length[3]');
+        $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[3]');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[3]');
+        $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'trim|required|min_length[3]|matches[password]');
+
+        if ($this->form_validation->run() == FALSE) {
+            $data['main_view'] = 'users/register_view';
+            $this->load->view('layouts/main', $data);
+        } else {
+            if ($this->user_model->create_user()) {
+                $this->session->set_flashdata('user_registered', 'User has been registered');
+                redirect('home/index');
+            } else {
+
+            }
+        }
+    }
+
     public function login()
     {
         $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[3]');
